7855c236593423fd66dcedce6cffe38e4808e8d5
okyash007 <helloyashverma@gmail.com>
2023-11-27 15:17:14 +0530

    bugs fix

--- src/home/News.jsx
+++ src/home/News.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
 import useFetchData from "../hooks/useFetchData";
 import { getRandomNumber } from "../helper";
 import styles from "./news.module.css";
+import { useSelector } from "react-redux";
 
 const News = () => {
   const data = useFetchData(
     "https://newsapi.org/v2/top-headlines?country=us&apiKey=ffd1b1a6599e42fdb4b6d6617d83b5a8"
   );
 
+  const store = useSelector(store => store.app)
+
   const displayData = data.data?.articles;
 
   if (!displayData) {
     return null;
   }
+
   const news = displayData[Math.floor(Math.random() * displayData.length)];
 
   return (
@@ -23,7 +27,11 @@ const News = () => {
           <p>{news.title}</p>
         </div>
       </div>
-      <div className={styles.bottom}>{news.content}<br/>{news.description}</div>
+      <div className={styles.bottom}>
+        {news.content}
+        <br />
+        {news.description}
+      </div>
     </div>
   );
 };
