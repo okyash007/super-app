c509928985a108f672d4b7c077a4e14359ee7355
okyash007 <helloyashverma@gmail.com>
2023-11-30 17:36:11 +0530

    added movies page

--- src/home/User.jsx
+++ src/home/User.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { Chip } from "../styled";
 import { selectTrue } from "../helper";
 import { useNavigate } from "react-router-dom";
+import pfp from '../assets/pfp.png'
 
 const User = () => {
   const store = useSelector((store) => store.app);
@@ -19,7 +20,7 @@ const User = () => {
     <div className={styles.box}>
       <div className={styles.pfp}>
         <img
-          src="https://s3-alpha-sig.figma.com/img/fb40/f748/ece2d5643cf2ffbd07a4d6221e0c51ff?Expires=1701648000&Signature=iZvbyEDVdfX53LC6eF9KT9hk61P1~IHhss8CyQKeZuN3UPlBUwIykLEzOdUL0Xn1wbbwWgkOXUV8TqH~PdrNR-jRFw-UAnuulPhFxLtKhNmb9meTyNm9s1G9HaegFqXdTXUCxOLEjZNQChd-q5mfZet2DrbTyVNYY7noKgUY0EvsDHXg23fTi0XZdEMt3qbP0vb8P06dJOpv17yDVGPRP9cKPEfxMzCG2wRCC8uXADZLuV40mzF1AVUiLdHE~dSi-XAIlsaPitIIK3uCbLfVYTzhZAFSs6noEa1UJ4M-tmrrx2gYA4W6TK5hihEjHSIIhWYOKs7ByKo5O-nmQrrb2g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          src={pfp}
           alt=""
         />
       </div>
@@ -32,7 +33,7 @@ const User = () => {
         <div className={styles.chips}>
           {chips.map((m) => (
             <Chip key={m.id} $bgColor="#9F94FF">
-              <p>{m.id}</p>
+              <p>{m.name}</p>
             </Chip>
           ))}
         </div>
--- src/movies/Movies.jsx
+++ src/movies/Movies.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { selectTrue } from "../helper";
 import useMovies from "../hooks/useMovies";
+import { Heading } from "../styled";
+import pfp from "../assets/pfp.png";
+import styles from "./movies.module.css";
+import Genre from "./genre";
 
 const Movies = () => {
   const store = useSelector((store) => store.app);
   const navigate = useNavigate();
 
-  console.log(useMovies("https://api.themoviedb.org/3/discover/movie?with_genres=53"))
+  const genre = selectTrue(store.genre);
+
+  if (!genre) {
+    return null;
+  }
 
   useEffect(() => {
     if (selectTrue(store.genre).length < 3) {
@@ -18,7 +26,28 @@ const Movies = () => {
     }
   }, []);
 
-  return <div>Movie</div>;
+  return (
+    <div className={styles.screen}>
+      <div className={styles.box}>
+        <div className={styles.header}>
+          <Heading $color="#72DB73" $fontSize="30px">
+            Super app
+          </Heading>
+          <Link to={"/"}>
+            <div className={styles.pfp}>
+              <img src={pfp} alt="" />
+            </div>
+          </Link>
+        </div>
+        <div className={styles.genre}>
+          <h2>Entertainment according to your choice</h2>
+          {genre.map((m) => (
+            <Genre key={m.id} genre={m} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Movies;
